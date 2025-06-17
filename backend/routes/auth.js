361efd64dd5aabe3030231957ffc55b4845f8c67
TakutoMorigaki <taku109ttaakkuu@gmail.com>
2025-06-17 21:22:26 +0900

    DBへのユーザー登録まで

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,12 +6,30 @@ const router = express.Router();
 
 router.post('/register', async (req, res) => {
     const { username, password } = req.body;
-})
 
-if(!username || !password){
-    return res.status(400).json({message: 'そのユーザー名は既に使われています'})
-}
+    if(!username || !password){
+        return res.status(400).json({message: '名前とパスワードを入力してください'});
+    }
 
-const hashedPassword = await bcrypt(password, 10);
+    try {
+        const existingUser = await User.findOne({username});
+        if (existingUser) {
+            return res.status(409).json({message: 'そのユーザー名は既に使われています'})
+        }
 
-const newUser = new User({ username, password: hashedPassword})
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = new User({ 
+            username,
+            password: hashedPassword
+        });
+        await newUser.save();
+
+        res.status(201).json({ message: 'ユーザー登録に成功しました'});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'サーバーエラーが発生しました'});
+    }
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config;
+const authRoutes = require('./routes/auth');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRoutes);
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('connected'))
 .catch((err) => console.error('connect error', err));
 
